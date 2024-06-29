0c262db6e5f48da396fc43d6783225429179ffcc
Pathetic{OTD} <50541050+PatheticOTD@users.noreply.github.com>
2024-06-29 10:20:36 +0300

    допилить инерцию и найти ее минимум

--- ultrakill.py
+++ ultrakill.py
@@ -1,40 +1,22 @@
-import random
-class RandomizedSet(object):
-
-    def __init__(self):
-        self.hashmap = {}
-        self.list_of_vals = []
-
-    def insert(self, val):
-        statement = val not in self.hashmap
-        if statement:
-            self.hashmap[val] = len(self.list_of_vals)
-            self.list_of_vals.append(val)
-        return statement
-
+def searchInsert(self, nums, target):
+    left = 0
+    right = len(nums)-1
+    while True:
+        middle = (right + left) // 2
+        if target == nums[left]:
+            return left
+        elif target == nums[right]:
+            return right
+        elif target == nums[middle]:
+            return middle
+        elif right - left == 1:
+            return left
         
-    def remove(self, val):
-        statement = val in self.hashmap
-        if statement:
-            self.hashmap[self.list_of_vals[-1]] = self.hashmap[val]
-            del self.hashmap[val]
-
-            self.list_of_vals[self.hashmap[self.list_of_vals[-1]]] = self.list_of_vals[-1]
-            self.list_of_vals.pop(-1)
-            
-
-        return statement
-
-    def getRandom(self):
-        return self.list_of_vals[random.randint(0, len(self.list_of_vals)-1)]
-
+        if target > nums[middle]:
+            left = middle
+        else:right = middle
+    
+    
+    
 if __name__ == '__main__':
-    rset = RandomizedSet()
-    print(rset.insert(1))
-    print(rset.insert(2))
-    print(rset.insert(3))
-    print(rset.insert(5))
-    print(rset.insert(7))
-    print(rset.insert(0))
-    print(rset.remove(1))
-    print(rset.getRandom())
+    print(productExceptSelf([1,2,3,4]))
